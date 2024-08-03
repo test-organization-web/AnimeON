669f58505d557e30b95d949dc55b65e4810a5e30
ivan.baturin <baturin.ivan9@gmail.com>
2024-08-03 12:37:45 +0300

    Add name of admin page

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,12 @@
+from django.conf import settings
+from django.contrib import admin
+
+
+admin.site.site_header = f'AnimeON {settings.PROJECT_VERSION}'
+admin.site.site_title = 'AnimeON'
+admin.site.index_title = 'Administration'
+
+
 class ReadOnlyPermissionsMixin:
     def has_add_permission(self, request, obj=None):
         return False
