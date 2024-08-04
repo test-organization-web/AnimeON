f3c10017ea7bed409086a530d123a0648a599c7f
ivan.baturin <baturin.ivan9@gmail.com>
2024-08-05 01:34:36 +0300

    Fixed get_count_episodes

--- apps/anime/serializers.py
+++ apps/anime/serializers.py
@@ -80,17 +80,6 @@ class VoiceoverSerializer(serializers.ModelSerializer):
         return f"{reverse('anime:get_anime_list')}?{get_params.urlencode()}"
 
 
-class VoiceoverPlayerSerializer(serializers.ModelSerializer):
-    value = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Voiceover
-        fields = ['value', 'url']
-
-    def get_value(self, obj: Voiceover):
-        return obj.team.name
-
-
 class ChildEpisodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Episode
@@ -143,7 +132,7 @@ class ResponseAnimeListSerializer(serializers.ModelSerializer):
         ]
 
     def get_count_episodes(self, obj: Anime):
-        return obj.count_episodes
+        return obj.episode_set.all().count()
 
     def get_year(self, obj: Anime):
         return obj.start_date.year
@@ -155,7 +144,7 @@ class ResponseAnimeSerializer(serializers.ModelSerializer):
                                         source='previewimage_set')
     genres = GenreSerializer(many=True)
     director = DirectorSerializer()
-    studio = StudioSerializer(many=True)
+    studio = StudioSerializer(many=True, read_only=True)
     episodes_release_schedule = ChildEpisodesReleaseScheduleSerializer(
         many=True, source='get_episodes_release_schedule')
     voiceovers = serializers.ListSerializer(child=VoiceoverSerializer(),
@@ -274,8 +263,8 @@ class ResponseAnimeRandomSerializer(serializers.ModelSerializer):
 
 
 class ResponseAnimeEpisodeSerializer(serializers.ModelSerializer):
-    voiceover = VoiceoverPlayerSerializer(source='voiceovers', many=True, read_only=True)
-    subtitles = VoiceoverPlayerSerializer(many=True, read_only=True)
+    voiceover = VoiceoverSerializer(source='voiceovers', many=True, read_only=True)
+    subtitles = VoiceoverSerializer(many=True, read_only=True)
 
     class Meta:
         model = Episode
